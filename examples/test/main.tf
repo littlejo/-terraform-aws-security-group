435145cdcbaaf34ddb379d8eebe864975b1c9369
Joseph Ligier <joseph.ligier@accenture.com>
2023-08-08 09:27:16 +0200

    Ability to add 2 protocols using a comma

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,12 +1,19 @@
-module "ssh" {
-  source = "../.."
-  name = "ssh"
-  description = "access to ssh"
+module "test" {
+  source      = "../.."
+  name        = "test"
+  description = "test"
   ingress = [
     {
-      description     = "ssh"
-      port            = 22
-      cidr_blocks     = ["0.0.0.0/0"]
-    }
+      description = "ssh"
+      port        = 22
+      cidr_blocks = ["0.0.0.0/0", "10.0.0.0/8"]
+      protocol    = "tcp"
+    },
+    {
+      description = "dns"
+      port        = 53
+      cidr_blocks = ["0.0.0.0/0", "10.0.0.0/8"]
+      protocol    = "tcp,udp"
+    },
   ]
 }
--- main.tf
+++ main.tf
@@ -1,18 +1,48 @@
 locals {
-  ingress = [
+  ingress_tmp = [
     for ingress in var.ingress :
-    {
-      from_port        = ingress.from_port == null ? ingress.port : ingress.from_port
-      to_port          = ingress.to_port == null ? ingress.port : ingress.to_port
-      protocol         = ingress.protocol
-      cidr_blocks      = ingress.cidr_blocks
-      ipv6_cidr_blocks = ingress.ipv6_cidr_blocks
-      description      = ingress.description
-      prefix_list_ids  = ingress.prefix_list_ids
-      self             = ingress.self
-      security_groups  = ingress.security_groups
-    }
+    strcontains(ingress.protocol, ",") ?
+    [
+      {
+        from_port        = ingress.from_port == null ? ingress.port : ingress.from_port
+        to_port          = ingress.to_port == null ? ingress.port : ingress.to_port
+        protocol         = split(",", ingress.protocol)[0]
+        cidr_blocks      = ingress.cidr_blocks
+        ipv6_cidr_blocks = ingress.ipv6_cidr_blocks
+        description      = ingress.description
+        prefix_list_ids  = ingress.prefix_list_ids
+        self             = ingress.self
+        security_groups  = ingress.security_groups
+      },
+      {
+        from_port        = ingress.from_port == null ? ingress.port : ingress.from_port
+        to_port          = ingress.to_port == null ? ingress.port : ingress.to_port
+        protocol         = split(",", ingress.protocol)[1]
+        cidr_blocks      = ingress.cidr_blocks
+        ipv6_cidr_blocks = ingress.ipv6_cidr_blocks
+        description      = ingress.description
+        prefix_list_ids  = ingress.prefix_list_ids
+        self             = ingress.self
+        security_groups  = ingress.security_groups
+      }
+    ]
+    :
+    [
+      {
+        from_port        = ingress.from_port == null ? ingress.port : ingress.from_port
+        to_port          = ingress.to_port == null ? ingress.port : ingress.to_port
+        protocol         = ingress.protocol
+        cidr_blocks      = ingress.cidr_blocks
+        ipv6_cidr_blocks = ingress.ipv6_cidr_blocks
+        description      = ingress.description
+        prefix_list_ids  = ingress.prefix_list_ids
+        self             = ingress.self
+        security_groups  = ingress.security_groups
+      }
+    ]
   ]
+
+  ingress = flatten(local.ingress_tmp)
 }
 
 resource "aws_security_group" "this" {
